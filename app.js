0068d0dd0b1152c55f357799fb84724e0e136b11
sjhuang26 <sjhuang26@gmail.com>
2018-05-13 19:51:50 -0400

    work

---
+++ app.js
@@ -0,0 +1,6 @@
+import {MDCRipple} from '@material/ripple';
+import {MDCTopAppBar} from '@material/top-app-bar/index';
+
+const fabRipple = new MDCRipple(document.querySelector('.mdc-fab'));
+const topAppBar = new MDCTopAppBar(document.querySelector('.mdc-top-app-bar'));
+const ripple = new MDCRipple(document.querySelector('.foo-button'));
